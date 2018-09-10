fa663effad4f0ec400cccab24804dd257227d863
Isaac Pierce <contact@isaacdpierce.com>
2018-09-09 23:28:10 -0700

    Server setup and test schema

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
+import { ApolloClient } from 'apollo-client';
+import { ApolloProvider } from 'react-apollo';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloLink } from 'apollo-link';
+import { HttpLink } from 'apollo-link-http';
+import { MeteorAccountsLink } from 'meteor/apollo';
 
 import App from '../../ui/App';
 
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    new MeteorAccountsLink(),
+    new HttpLink({
+      uri: '/graphql',
+    }),
+  ]),
+  cache: new InMemoryCache(),
+});
+
+const ApolloApp = () => (
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+);
+
 Meteor.startup(() => {
-  render(<App />, document.getElementById('app'));
+  render(<ApolloApp />, document.getElementById('app'));
 });
---
+++ imports/startup/server/register-api.js
@@ -0,0 +1,40 @@
+import { ApolloServer, gql } from 'apollo-server-express';
+import { WebApp } from 'meteor/webapp';
+import { getUser } from 'meteor/apollo';
+
+import ResolutionsSchema from '../../api/resolutions/Resolutions.graphql';
+
+const testSchema = `
+type Query {
+  hi: String
+}
+`;
+
+const typeDefs = [testSchema, ResolutionsSchema];
+
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello from resolvers';
+    },
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: async ({ req }) => ({
+    user: await getUser(req.headers.authorization),
+  }),
+});
+
+server.applyMiddleware({
+  app: WebApp.connectHandlers,
+  path: '/graphql',
+});
+
+WebApp.connectHandlers.use('/graphql', (req, res) => {
+  if (req.method === 'GET') {
+    res.end();
+  }
+});
